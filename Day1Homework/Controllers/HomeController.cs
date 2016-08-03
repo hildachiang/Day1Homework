5ae823212c45fc4cccad530fdbe911df1407f159
WantingChiang <WantingChiang@WANTING-PC>
2016-08-03 23:43:14 +0800

    加入專案檔案。

---
+++ Day1Homework/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Day1Homework.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChildAction()
+        {
+            var model = new List<AccountBookModel>();
+            model.Add(new AccountBookModel() { ClassType = "支出", AccountDate = "2016-01-01", BillingAmount = 300 });
+            model.Add(new AccountBookModel() { ClassType = "支出", AccountDate = "2016-01-02", BillingAmount = 1600 });
+            model.Add(new AccountBookModel() { ClassType = "支出", AccountDate = "2016-01-03", BillingAmount = 800 });
+
+            return View(model);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Day1Homework/ViewModel/AccountBookModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day1Homework.ViewModel
+{
+    public class AccountBookModel
+    {
+        public string ClassType{ get; set; }
+        public string AccountDate{ get; set; }
+
+        public double BillingAmount{ get; set; }
+        public string Note{ get; set; }
+    }
+}
