05bb7798ebd2cb5af8c3b5d56fb5754bfd5430ec
WantingChiang <WantingChiang@WANTING-PC>
2016-08-10 23:28:47 +0800

    加入view & viewmodel

---
+++ Day1Homework/ViewModel/CreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+namespace Day1Homework.ViewModel
+{
+   public class CreateViewModel
+    {
+        public AccountBookModel AccountBookModel { get; set; }
+        public List<SelectListItem> CategoryOptions { get; set; }
+    }
+}
