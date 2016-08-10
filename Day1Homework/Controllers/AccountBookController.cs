2e1c80943cc4af7c361867b3954200ae6978eda8
WantingChiang <WantingChiang@WANTING-PC>
2016-08-10 23:33:19 +0800

    簽入controller，childaction畫面上的類別尚待修改

---
+++ Day1Homework/Controllers/AccountBookController.cs
@@ -0,0 +1,59 @@
+﻿using Day1Homework.Models;
+using Day1Homework.Repositories;
+using Day1Homework.Service;
+using Day1Homework.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1Homework.Controllers
+{
+    public class AccountBookController : Controller
+    {
+        private readonly AccountBookService _accountBookSvc;
+        public AccountBookController()
+        {
+            var unitOfWork = new EFUnitOfWork();
+            _accountBookSvc = new AccountBookService(unitOfWork);
+        }
+
+        // GET: AccountBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChildAction()
+        {
+            return View(_accountBookSvc.Lookup());
+        }
+
+        public ActionResult Create()
+        {
+            var categoryOptions = new List<SelectListItem>{
+                        new SelectListItem { Text = "請選擇", Value="" },
+                        new SelectListItem { Text = "支出", Value="支出" },
+                        new SelectListItem { Text = "收入", Value="收入" }
+                    };
+
+            return View(new CreateViewModel { CategoryOptions = categoryOptions, AccountBookModel = new AccountBookModel() });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _accountBookSvc.Add(model.AccountBookModel);
+                _accountBookSvc.Save();
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+    }
+
+}
