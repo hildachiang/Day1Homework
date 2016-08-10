406ebc17374cd5546a1f645d16a8082e462b55e2
WantingChiang <WantingChiang@WANTING-PC>
2016-08-10 23:31:23 +0800

    執行異常，linq裡面不能下tostring，改成先用asenumerable

--- Day1Homework/Models/AccountBookService.cs
+++ Day1Homework/Models/AccountBookService.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Data.Objects.SqlClient;
 using System.Web;
 using Day1Homework.Models;
 using Day1Homework.Repositories;
 using Day1Homework.ViewModel;
 
-namespace Day1Homework.Service 
+namespace Day1Homework.Service
 {
     public class AccountBookService : Repository<AccountBook>
     {
@@ -21,40 +22,39 @@ namespace Day1Homework.Service
         public IEnumerable<AccountBookModel> Lookup()
         {
             var source = _accountRep.LookupAll();
-            var result = source.Select(ab => new AccountBookModel()
+            return source.AsEnumerable().Select(d => new AccountBookModel()
             {
-                AccountDate = ab.Dateee.ToString("yyyy/MM/dd"),
-                BillingAmount = ab.Amounttt,
-                ClassType = ab.Categoryyy.ToString(),
-                Note = ab.Remarkkk
-            }).ToList();
-            return result;
+                AccountDate = d.Dateee.ToString("yyyy/MM/dd"),
+                BillingAmount = d.Amounttt,
+                ClassType = d.Categoryyy.ToString(),
+                Note = d.Remarkkk
+            }
+            ).ToList();
+
         }
 
 
         public void Add(AccountBookModel accountBook)
         {
-            var result = new AccountBook()
+            Add(new AccountBook()
             {
                 Amounttt = (Int32)accountBook.BillingAmount,
                 Categoryyy = Int32.Parse(accountBook.ClassType),
                 Dateee = DateTime.Parse(accountBook.AccountDate),
                 Remarkkk = accountBook.Note
-            };
-            Add(result);
+            });
         }
 
         public void Add(AccountBook accountBook)
         {
-            var newAccountBook = new AccountBook()
+            _accountRep.Create(new AccountBook()
             {
                 Id = Guid.NewGuid(),
                 Amounttt = accountBook.Amounttt,
                 Categoryyy = accountBook.Categoryyy,
                 Dateee = accountBook.Dateee,
                 Remarkkk = accountBook.Remarkkk
-            };
-            _accountRep.Create(newAccountBook);
+            });
         }
 
         public void Save()
