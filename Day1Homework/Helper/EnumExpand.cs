6cb29bfd30e616e73d17ff57f270401a867b03f6
WantingChiang <WantingChiang@WANTING-PC>
2016-08-13 22:32:00 +0800

    新增取得enum display name的擴充方法

---
+++ Day1Homework/Helper/EnumExpand.cs
@@ -0,0 +1,30 @@
+﻿using Day1Homework.MyEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Day1Homework.Helper
+{
+    public static class EnumExpand
+    {
+        public static string GetDisplayName(this CategoryEnum myEnum)
+        {
+            var member = (myEnum.GetType().GetMember(myEnum.ToString()))[0];
+            var attribute = (DisplayAttribute)(member.GetCustomAttributes(typeof(DisplayAttribute), false))[0];
+            return attribute.GetName();
+        }
+
+        public static string GetDisplayName(this CategoryEnum myEnum, int category)
+        {
+            string value = category.ToString();
+            CategoryEnum myCategoryEnum;
+            if (Enum.TryParse(value, true, out myCategoryEnum))
+            {
+                return GetDisplayName(myCategoryEnum);
+            }
+            else
+                return "N/A";
+        }
+    }
+}
