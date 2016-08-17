3e8d20c32fa6671b0746f013a8efd02da868b3a9
WantingChiang <WantingChiang@WANTING-PC>
2016-08-17 22:43:54 +0800

    1.拿掉未使用的擴充方法 2.為留下的擴充方法新增註解

--- Day1Homework/Helper/EnumExpand.cs
+++ Day1Homework/Helper/EnumExpand.cs
@@ -5,22 +5,16 @@ namespace Day1Homework.Helper
 {
     public static class EnumExpand
     {
-        public static string GetDisplayName(this CategoryEnum myEnum)
+        /// <summary>
+        /// 取得CategoryEnum的display name
+        /// </summary>
+        /// <param name="myEnum">欲取得display name的Enum</param>
+        /// <returns></returns>
+        public static string GetDisplayName(this Enum myEnum)
         {
             var member = (myEnum.GetType().GetMember(myEnum.ToString()))[0];
             var attribute = (DisplayAttribute)(member.GetCustomAttributes(typeof(DisplayAttribute), false))[0];
             return attribute.GetName();
-        }
-
-        public static string GetDisplayName(this CategoryEnum myEnum, int category)
-        {
-            CategoryEnum myCategoryEnum;
-            if (Enum.TryParse(category.ToString(), true, out myCategoryEnum))
-            {
-                return GetDisplayName(myCategoryEnum);
-            }
-            else
-                return "N/A";
         }
     }
 }
