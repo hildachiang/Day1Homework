156575b92f6f84c79bcc15cbef8cee8371dd1569
WantingChiang <WantingChiang@WANTING-PC>
2016-08-27 00:38:11 +0800

    ▪所有欄位必填 ▪「金額」只能輸入正整數

--- Day1Homework/ViewModel/AccountBookModel.cs
+++ Day1Homework/ViewModel/AccountBookModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,14 @@ namespace Day1Homework.ViewModel
 {
     public class AccountBookModel
     {
+        [Required]
         public string ClassType{ get; set; }
+        [Required]
         public string AccountDate{ get; set; }
-
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "「金額」只能輸入正整數")]
         public double BillingAmount{ get; set; }
+        [Required]
         public string Note{ get; set; }
     }
 }
